8114931166d49310e5b1f8f5962b66d60cbc119b
Alexey Zbinyakov <alexey@DESKTOP-FFV923G>
2023-06-13 16:08:21 +0700

    set better output, add random params

--- predict.py
+++ predict.py
@@ -1,35 +1,49 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
+#MODELSIZE = 'large'
+MODELSIZE = 'medium'
+#MODELSIZE = 'small'
+
+
 from cog import BasePredictor, Input, Path
 
 import torch
 from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 import os
+import random
+
+
+import calendar;
+import time;
+
 os.environ['LD_LIBRARY_PATH'] = '' #need to unset incompatible cuda libs
 os.environ['XDG_CACHE_HOME']="/src/.container-cache" #need to chache locally
 
 print(torch.version.cuda)
 class Predictor(BasePredictor):
     def setup(self):
+        self.modelSize = MODELSIZE
         """Load the model into memory to make running multiple predictions efficient"""
-        self.model = MusicGen.get_pretrained('medium')
+        self.model = MusicGen.get_pretrained(self.modelSize)
         # self.model = torch.load("./weights.pth")
 
     def predict(
         self,
 		prompt:str = 'happy birthday polka',
-        duration:int = 10
+        duration:int = 10,
+        seed:int = random.randint(0, 1000000000)
     ) -> Path:
         """Run a single prediction on the model"""
         descriptions = [prompt]
-
+        random.seed(seed)
+        torch.manual_seed(seed)
         self.model.set_generation_params(duration=duration)  # generate 8 seconds.
-        wav = self.model.generate(descriptions)  # generates 3 samples.
-        for idx, one_wav in enumerate(wav):
-            # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
-            audio_write(f'{idx}', one_wav.cpu(), self.model.sample_rate, strategy="loudness")
+        wav = self.model.generate(descriptions, progress = True)  # generates 3 samples.
+        one_wav = wav[0]
+        ts = calendar.timegm(time.gmtime())
+        audio_write(f'out/{prompt.replace(" ", "_")}-{self.modelSize}_{seed}', one_wav.cpu(), self.model.sample_rate, strategy="loudness")
                                                                                       
         # processed_input = preprocess(image)
         # output = self.model(processed_image, scale)
