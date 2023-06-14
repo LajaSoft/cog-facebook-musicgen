5b2cdd56ade95d512ca42e804abccd2ed59171be
melkor <melkor@example.com>
2023-06-14 03:23:45 +0300

    add continues generation

--- predict.py
+++ predict.py
@@ -2,8 +2,9 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
 #MODELSIZE = 'large'
-MODELSIZE = 'medium'
+#MODELSIZE = 'medium'
 #MODELSIZE = 'small'
+MODELSIZE = 'melody'
 
 
 from cog import BasePredictor, Input, Path
@@ -16,11 +17,25 @@ import random
 
 import calendar;
 import time;
+PART_LEN = 25 # reduce to get less vram (may be), and may be to get better results
+CUT_LEN = 20 # how much of last part to use for continuation (less this number - less glue quality, I guess. More - longer render, for sure) shall be less than PART_LEN!
 
 os.environ['LD_LIBRARY_PATH'] = '' #need to unset incompatible cuda libs
 os.environ['XDG_CACHE_HOME']="/src/.container-cache" #need to chache locally
 
 print(torch.version.cuda)
+
+def continue_melody(model, prompt, rate: int, descriptions = [], duration = PART_LEN, seed = random.randint(0, 1000000000)):
+    # print current torch seed
+    # torch.manual_seed(seed)
+    print("seed:", torch.seed())
+    model.set_generation_params(duration=duration)
+    part = model.generate_continuation(prompt=prompt, prompt_sample_rate = rate,
+                              descriptions = descriptions,
+                              progress = True)
+    return part
+
+
 class Predictor(BasePredictor):
     def setup(self):
         self.modelSize = MODELSIZE
@@ -35,19 +50,61 @@ class Predictor(BasePredictor):
         seed:int = random.randint(0, 1000000000)
     ) -> Path:
         """Run a single prediction on the model"""
+        total_duration = 0
+        if (duration > PART_LEN):
+            total_duration = duration
+            duration = PART_LEN
+
         descriptions = [prompt]
         torch.manual_seed(seed)
         self.model.set_generation_params(duration=duration)  
-        wav = self.model.generate(descriptions, progress = True)  # generates .
-        one_wav = wav[0]
-        ts = calendar.timegm(time.gmtime())
+        all_parts = self.model.generate(descriptions, progress = True)  # generates .
+        current_duration = duration
+        first_wav = all_parts[0]
+        collected_parts = first_wav
+        if total_duration:
+            current_duration = 0
+            last_wav = first_wav
+            while current_duration < total_duration:
+                part_to_continue = collected_parts[..., -int(PART_LEN * self.model.sample_rate):]
+                part_to_continue = part_to_continue[..., -int(CUT_LEN * self.model.sample_rate):]
+                part_to_continue_length = part_to_continue.shape[1]
+
+                print ("part_to_continue", part_to_continue.shape)
+                #all_parts[0] = part_to_continue
+                last_wav = continue_melody(
+                    model = self.model,
+                    prompt = part_to_continue,
+                    rate=self.model.sample_rate,
+                    descriptions=descriptions,
+                    duration = min(PART_LEN, total_duration - current_duration + CUT_LEN),
+                    seed = seed
+                )[0]
+                print ("last_wav before", last_wav.shape)
+                print ("CUT LEN, rate", CUT_LEN, self.model.sample_rate)
+                last_wav = last_wav[...,part_to_continue_length:]
+                print ("last_wav", last_wav.shape)
+                collected_parts = torch.cat([collected_parts, last_wav], dim = 1)
+                current_duration = collected_parts.shape[1] / self.model.sample_rate
+                print ("current_duration", current_duration)
+        else:
+            total_duration = duration
         audio_write(
-                    f'out/{prompt.strip().replace(" ", "_")}-{self.modelSize}_{seed}-{duration}s',
-                    one_wav.cpu(), 
-                    self.model.sample_rate, 
-                    strategy="loudness",
-                    loudness_compressor=True
-                    )
+                f'out/{prompt.strip().replace(" ", "_").replace(":","=")}-{self.modelSize}_{seed}-{int(collected_parts.shape[1]/self.model.sample_rate)}s',
+                collected_parts.cpu(), 
+                self.model.sample_rate, 
+                strategy="loudness",
+                loudness_compressor=True
+        )
+
+        # for i,part in all_parts:
+        #     audio_write(
+        #                 f'out/{prompt.strip().replace(" ", "_")}-{self.modelSize}_{seed}-{duration}s-{i}',
+        #                 part.cpu(), 
+        #                 self.model.sample_rate, 
+        #                 strategy="loudness",
+        #                 loudness_compressor=True
+        #                 )
                                                                                       
         # processed_input = preprocess(image)
         # output = self.model(processed_image, scale)
